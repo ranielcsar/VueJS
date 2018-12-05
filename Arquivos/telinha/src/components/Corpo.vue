436f65b99ac78aa264cc3bab299421eb790198b4
Raniel <whyfck@gmail.com>
2018-12-05 10:28:48 -0200

    Update Corpo.vue

--- Arquivos/telinha/src/components/Corpo.vue
+++ Arquivos/telinha/src/components/Corpo.vue
@@ -1,36 +1,57 @@
 <template>
-   <ul class="lista">
-      <li v-for = "itens in itemLista">
-         {{ itens.item }}
-      </li>
-   </ul>   
+  <div class="news">
+    <img :src= "imagem" class="foto">
+    <div><p>{{ descricao }}</p></div>
+    <a :href= "link">Saiba mais...</a>
+  </div>  
 </template>
 
 <script type="text/javascript">
 
-   export default {
-      name: 'corpo',
+export default {
+  props: [ 
+    'imagem',
+    'descricao',
+    'link'
+  ],
 
-      data() 
-      {
-         return {
-            itemLista:
-            [
-               { item: '1º item :D' },
-               { item: '2º item :D' },
-               { item: '3º item :D' },
-               { item: '4º item :D' }
-            ]
-         }         
-      }
-   }  
+  data()
+  {
+    this.imagem = 'https://homemaranha60s.files.wordpress.com/2012/07/4.jpg',
+    this.descricao = 'Homem-aranha 60s',
+    this.link = '#'
+  }  
+}
 
 </script>
 
 <style type="text/css">
    
-   .lista {
-      color: #999999;
-   }   
+.news {
+   margin: 10px 2%;   
+   float: left;
+   width: calc(100% - 15px);   
+   border: 1px solid black;   
+   border-radius: 0.5em;
+   overflow: auto;
+}
 
+.news a {
+   padding: 10px;
+   text-decoration: none;
+}  
+
+.news p {
+   padding: 0.5em;
+   text-align: center;
+}
+
+.foto {    
+    width: 100%;    
+    height: auto;        
+}
+
+    .foto:after,
+    .foto:before
+    { content: ""; clear: both; }
 </style>
