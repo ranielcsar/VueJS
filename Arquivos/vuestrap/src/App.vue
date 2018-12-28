2f00c7a187689962a09eaae0efe5d420545236ca
Raniel <whyfck@gmail.com>
2018-12-28 11:46:16 -0300

    vue + bootstrap :D

---
+++ Arquivos/vuestrap/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="principal">
+    <alerta></alerta>
+  </div>
+</template>
+
+<script>
+
+import alerta from '../src/components/Alerta.vue'
+
+export default {
+  name: 'app',
+
+  components:
+  {
+    alerta
+  }
+}
+
+</script>
+
+<style>
+
+#principal {
+  padding: 0;
+  margin-left: 10%;
+  box-sizing: border-box;
+  width: 80%;
+  font-size: 15px;
+}
+
+</style>
---
+++ Arquivos/vuestrap/src/components/Alerta.vue
@@ -0,0 +1,31 @@
+<template>
+   <div>
+      <b-alert show v-for="variante in variantes" :variant="variante.valor">
+         {{ variante.valor }} alert
+      </b-alert>
+   </div>
+</template>
+
+<script>
+   
+   export default {
+
+      data: function()
+      {
+         return {
+            variantes:
+            [
+               {valor: 'primary'},
+               {valor: 'secondary'},
+               {valor: 'success'},
+               {valor: 'danger'},
+               {valor: 'warning'},
+               {valor: 'info'},
+               {valor: 'light'}, 
+               {valor: 'dark'}
+            ]            
+         }
+      }
+   }
+
+</script>
