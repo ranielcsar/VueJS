3ace42ff1a6325915ba8c45d4beb728c621b3e88
Raniel <whyfck@gmail.com>
2019-02-04 14:09:55 -0300

    :]

---
+++ Jogo/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <meuCanvas :x = "x" :y = "y"/>
+  </div>
+</template>
+
+<script>
+
+import meuCanvas from './components/meuCanvas.vue';
+
+export default {
+  name: 'app',
+
+  components: { meuCanvas },
+
+  data() {
+    return {
+      x: 10,
+      y: 10
+    }
+  },
+
+  methods: {
+    mover(evento)
+    {
+      switch (evento.keyCode)
+      {
+        case 37:
+          this.x -= 3;
+          break;
+        case 38:
+          this.y -= 3;
+          break;
+        case 39:
+          this.x += 3;
+          break;
+        case 40:
+          this.y += 3;
+          break;
+      }
+    }
+  },
+
+  mounted() { addEventListener("keydown", this.mover); }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Jogo/src/components/meuCanvas.vue
@@ -0,0 +1,43 @@
+<template>
+   <canvas width="850" height="590" class="canvas"></canvas>
+</template>
+
+<script>
+   
+export default {
+   props: { x: Number, y: Number },
+
+   methods:
+   {
+      draw()
+      {
+         this.lapis.clearRect(0, 0, 850, 590)
+         this.lapis.beginPath();
+         this.quadrado();
+      },
+
+      quadrado()
+      {
+         this.lapis.fillStyle = "#07fdb5";
+         this.lapis.fillRect(this.x, this.y, 50, 50);
+      }
+   },
+
+   mounted()
+   {
+      this.lapis = this.$el.getContext('2d');
+
+      setInterval(this.draw, 5);
+   }
+}
+
+</script>
+
+<style scoped>
+
+.canvas { 
+   background-color: #2d3030;
+   margin: 0 15%;
+}
+
+</style>
